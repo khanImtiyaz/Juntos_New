589710032ece9c15b6fdaa19c4d4db3cb085782c
Imtiyaz Ahemad <imtiyaz.ahemad@mobiloittegroup.com>
2017-12-18 14:46:10 +0530

    Delete Migrations

--- Static_Model/migrations/0004_auto_20171218_0908.py
+++ Static_Model/migrations/0004_auto_20171218_0908.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('Static_Model', '0003_juntoscontactusemail'),
+        ('Static_Model', '0001_initial'),
     ]
 
     operations = [
