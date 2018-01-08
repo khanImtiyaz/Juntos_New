9102200847fa1ff36b2076c8736b87299db07686
Imtiyaz Ahemad <imtiyaz.ahemad@mobiloittegroup.com>
2018-01-08 12:43:35 +0530

    Add cart Error Resolved

--- Juntos/views.py
+++ Juntos/views.py
@@ -444,9 +444,9 @@ class ViewCart(View):
 	def post(self,request):
 		user = request.user
 		if user.is_authenticated and user.is_customer:
-			total_price = 0
+			total_price = 0.0
 			product_cart = Cart.objects.filter(user=user)
-			total_price = product_cart.aggregate(Sum('price'))['price__sum']
+			total_price = product_cart.aggregate(Sum('price'))['price__sum'] if product_cart.aggregate(Sum('price'))['price__sum'] else 0.0
 			total_price = total_price + (float(total_price) * 18)/100
 			return render(request,'new_view_cart.html',{"all_cart":product_cart,"total_price":total_price})
 		else:
